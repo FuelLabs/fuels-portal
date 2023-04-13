ad90070be8702917bccac8a622d358abdaedcc44
Matt Auer <mattauer@umich.edu>
2023-04-13 16:23:04 -0500

    feat: add install popup and control to fuel network card

--- packages/app/src/systems/Bridge/components/FuelNetworkCard/FuelNetworkCard.tsx
+++ packages/app/src/systems/Bridge/components/FuelNetworkCard/FuelNetworkCard.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
+
+import { FuelInstallPopup } from '../FuelInstallPopup';
 import { NetworkCard } from '../NetworkCard';
 
-import { useConnectFuel, useFuelWallet } from '~/systems/Core/hooks';
+import { useConnectFuel, useFuel, useFuelWallet } from '~/systems/Core/hooks';
 
 type FuelNetworkCardProps = {
   heading: string;
@@ -9,17 +12,28 @@ type FuelNetworkCardProps = {
 export const FuelNetworkCard = ({ heading }: FuelNetworkCardProps) => {
   const fuelWallet = useFuelWallet();
   const connectFuel = useConnectFuel();
+  const fuel = useFuel();
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setPopupOpen(!popupOpen);
+  };
+
+  const onConnectFunc = fuel ? connectFuel.mutate : togglePopup;
 
   return (
-    <NetworkCard
-      network="Fuel"
-      networkImageUrl="public/fuel-logo.svg"
-      heading={heading}
-      address={
-        fuelWallet.data ? fuelWallet.data.address.toAddress() : undefined
-      }
-      isLoading={connectFuel.isLoading}
-      onConnect={() => connectFuel.mutate()}
-    />
+    <>
+      <NetworkCard
+        network="Fuel"
+        networkImageUrl="public/fuel-logo.svg"
+        heading={heading}
+        address={
+          fuelWallet.data ? fuelWallet.data.address.toAddress() : undefined
+        }
+        isLoading={connectFuel.isLoading}
+        onConnect={() => onConnectFunc()}
+      />
+      <FuelInstallPopup open={popupOpen} />
+    </>
   );
 };
