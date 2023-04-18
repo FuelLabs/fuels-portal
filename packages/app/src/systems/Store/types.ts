baefa5572397ed7a2e042ac72fb12710ac574df1
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-04-18 01:02:04 -0300

    feat: add infra xstate

---
+++ packages/app/src/systems/Store/types.ts
@@ -0,0 +1,40 @@
+import type { StoreClass } from '@fuels-portal/store';
+
+// import type { AccountsMachine } from '../Account';
+// import type { AssetsMachine } from '../Asset';
+// import type {
+//   TransactionRequestMachine,
+//   MessageRequestMachine,
+//   ConnectRequestMachine,
+//   AddAssetMachine,
+// } from '../DApp';
+// import type { NetworksMachine } from '../Network';
+// import type { OverlayMachine } from '../Overlay';
+// import type { UnlockMachine } from '../Unlock';
+
+export enum Services {
+  account = 'account',
+  accounts = 'accounts',
+  networks = 'networks',
+  assets = 'assets',
+  overlay = 'overlay',
+  unlock = 'unlock',
+  txRequest = 'txRequest',
+  msgRequest = 'msgRequest',
+  connectRequest = 'connectRequest',
+  addAssetRequest = 'addAssetRequest',
+}
+
+export type StoreMachines = {
+  // accounts: AccountsMachine;
+  // networks: NetworksMachine;
+  // assets: AssetsMachine;
+  // overlay: OverlayMachine;
+  // unlock: UnlockMachine;
+  // txRequest: TransactionRequestMachine;
+  // msgRequest: MessageRequestMachine;
+  // connectRequest: ConnectRequestMachine;
+  // addAssetRequest: AddAssetMachine;
+};
+
+export type Store = StoreClass<StoreMachines>;
