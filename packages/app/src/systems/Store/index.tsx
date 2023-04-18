631e182d2bc9e4ad8303cd358e1e37568124b958
Luiz Felipe Bolsoni Gomes <felipebolsonigomes@gmail.com>
2023-04-18 01:47:27 -0300

    feat: add xstate , store , overlay

--- packages/app/src/systems/Store/index.tsx
+++ packages/app/src/systems/Store/index.tsx
@@ -1,24 +1,10 @@
 import { createStore } from '@fuels-portal/store';
 
-// import { accountEvents } from '../Account/events';
-// import { accountsMachine } from '../Account/machines';
-// import { assetEvents, assetsMachine } from '../Asset';
-// import {
-//   transactionRequestMachine,
-//   connectRequestMachine,
-//   messageRequestMachine,
-//   addAssetRequestMachine,
-// } from '../DApp';
-// import { requestEvents } from '../DApp/events';
-// import { networkEvents } from '../Network/events';
-// import { networksMachine } from '../Network/machines';
-// import { overlayMachine } from '../Overlay';
-// import { overlayEvents } from '../Overlay/events';
-// import { unlockMachine } from '../Unlock';
-// import { unlockEvents } from '../Unlock/events';
+import { overlayMachine } from '../Overlay';
+import { overlayEvents } from '../Overlay/events';
 
 import type { StoreMachines } from './types';
-// import { Services } from './types';
+import { Services } from './types';
 
 export * from './types';
 
@@ -28,21 +14,8 @@ export const store$ = createStore<StoreMachines>({
 });
 
 export const store = store$
-  // .addMachine(Services.overlay, () => overlayMachine)
-  // .addMachine(Services.accounts, () => accountsMachine)
-  // .addMachine(Services.networks, () => networksMachine)
-  // .addMachine(Services.assets, () => assetsMachine)
-  // .addMachine(Services.unlock, () => unlockMachine)
-  // .addMachine(Services.msgRequest, () => messageRequestMachine)
-  // .addMachine(Services.connectRequest, () => connectRequestMachine)
-  // .addMachine(Services.txRequest, () => transactionRequestMachine)
-  // .addMachine(Services.addAssetRequest, () => addAssetRequestMachine)
-  // .addHandlers(accountEvents)
-  // .addHandlers(networkEvents)
-  // .addHandlers(assetEvents)
-  // .addHandlers(overlayEvents)
-  // .addHandlers(unlockEvents)
-  // .addHandlers(requestEvents)
+  .addMachine(Services.overlay, () => overlayMachine)
+  .addHandlers(overlayEvents)
   .setup();
 
 export const { StoreProvider } = store;
