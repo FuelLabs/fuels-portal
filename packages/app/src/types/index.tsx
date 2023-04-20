10fa7f9ac31d543c58deb8a792dbf2d7606c1ccc
Matt <54373384+matt-user@users.noreply.github.com>
2023-04-20 17:38:16 -0500

    refactor: header (#21)
    * refactor: header
    
    * refactor: css objs
    
    * refactor: implement review suggestions

--- packages/app/src/types/index.tsx
+++ packages/app/src/types/index.tsx
@@ -1,4 +1,10 @@
+// TODO the urls will have to be changed
 export enum Pages {
   'home' = '/',
   'bridge' = '/bridge',
+  'developers' = '/developers',
+  'community' = '/community',
+  'labs' = '/labs',
+  'explorer' = '/explorer',
+  'ecosystem' = '/ecosystem',
 }
