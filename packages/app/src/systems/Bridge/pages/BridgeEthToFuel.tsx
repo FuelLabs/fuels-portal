2ce200f4629b756cfab567b251f4959720f68f94
Matt Auer <mattauer@umich.edu>
2023-04-21 15:26:20 -0500

    feat: deposit and withdraw tab

--- packages/app/src/systems/Bridge/pages/BridgeEthToFuel.tsx
+++ packages/app/src/systems/Bridge/pages/BridgeEthToFuel.tsx
@@ -1,4 +1,4 @@
-import { Card, Stack } from '@fuel-ui/react';
+import { Stack } from '@fuel-ui/react';
 
 import ethLogoSrc from '../../../../public/eth-logo.svg';
 import fuelLogoSrc from '../../../../public/fuel-logo.svg';
@@ -6,25 +6,21 @@ import { NetworkConnectCard } from '../components';
 
 export const BridgeEthToFuel = () => {
   return (
-    <Card>
-      <Card.Body>
-        <Stack gap="$4">
-          <NetworkConnectCard
-            networkName={'Ethereum'}
-            networkImageUrl={ethLogoSrc}
-            label={'From'}
-            isConnecting={false}
-            onConnect={() => {}}
-          />
-          <NetworkConnectCard
-            networkName="Fuel"
-            networkImageUrl={fuelLogoSrc}
-            label="To"
-            isConnecting={false}
-            onConnect={() => {}}
-          />
-        </Stack>
-      </Card.Body>
-    </Card>
+    <Stack gap="$4">
+      <NetworkConnectCard
+        networkName={'Ethereum'}
+        networkImageUrl={ethLogoSrc}
+        label={'From'}
+        isConnecting={false}
+        onConnect={() => {}}
+      />
+      <NetworkConnectCard
+        networkName="Fuel"
+        networkImageUrl={fuelLogoSrc}
+        label="To"
+        isConnecting={false}
+        onConnect={() => {}}
+      />
+    </Stack>
   );
 };
