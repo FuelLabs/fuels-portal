0c2ee22a164a198dec825dd7506998e4fa7c0c0d
Matt Auer <mattauer@umich.edu>
2023-05-18 10:17:05 -0400

    feat: bridge tx overview

---
+++ packages/app/src/systems/Bridge/components/BridgeTxOverview/BridgeTxOverview.stories.tsx
@@ -0,0 +1,36 @@
+import { cssObj } from '@fuel-ui/css';
+import { Box } from '@fuel-ui/react';
+import { bn } from 'fuels';
+
+import ethImage from '../../../../../public/eth-logo.svg';
+import fuelImage from '../../../../../public/fuel-logo.svg';
+
+import { BridgeTxOverview } from './BridgeTxOverview';
+
+export default {
+  component: BridgeTxOverview,
+  title: 'BridgeTxOverview',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Usage = () => {
+  return (
+    <Box.Flex align="center" justify="center" css={styles.storybook}>
+      <BridgeTxOverview
+        transactionId={bn(1234567876543456)}
+        age="5 days ago"
+        isDeposit={true}
+        asset={{ imageUrl: ethImage, assetSymbol: 'ETH', assetAmount: '1.500' }}
+      />
+    </Box.Flex>
+  );
+};
+
+const styles = {
+  storybook: cssObj({
+    margin: '20px',
+    width: '348px',
+  }),
+};
