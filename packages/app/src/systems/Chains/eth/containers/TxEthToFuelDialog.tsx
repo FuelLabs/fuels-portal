ad9f0ebb075be7fe2300f70382a01413d52ed488
Matt Auer <mattauer@umich.edu>
2023-05-22 11:56:32 -0400

    feat: add more to eth to fuel dialog

--- packages/app/src/systems/Chains/eth/containers/TxEthToFuelDialog.tsx
+++ packages/app/src/systems/Chains/eth/containers/TxEthToFuelDialog.tsx
@@ -1,4 +1,13 @@
-import { Dialog, Icon, IconButton } from '@fuel-ui/react';
+import { cssObj } from '@fuel-ui/css';
+import {
+  Box,
+  Button,
+  Dialog,
+  Icon,
+  IconButton,
+  Input,
+  Text,
+} from '@fuel-ui/react';
 
 import { BridgeSteps } from '../../../Bridge/components';
 import { useTxEthToFuel } from '../hooks';
@@ -14,18 +23,35 @@ export function TxEthToFuelDialog() {
   return (
     <>
       <Dialog.Heading>
-        Transaction
-        <IconButton
-          data-action="closed"
-          variant="link"
-          icon={<Icon icon="X" color="gray8" />}
-          aria-label="Close unlock window"
-          onPress={handlers.close}
-        />
+        <Box.Flex justify="space-between">
+          Transaction
+          <IconButton
+            data-action="closed"
+            variant="link"
+            icon={<Icon icon="X" color="gray8" />}
+            aria-label="Close unlock window"
+            onPress={handlers.close}
+          />
+        </Box.Flex>
       </Dialog.Heading>
       <Dialog.Description>
-        <BridgeSteps steps={steps} />
+        <Box.Stack gap="$1">
+          <Text css={styles.header}>Status</Text>
+          <BridgeSteps steps={steps} />
+          <Text css={styles.header}>Get notified when it settles</Text>
+          <Box.Flex>
+            <Input />
+            <Button>Notify me</Button>
+          </Box.Flex>
+        </Box.Stack>
       </Dialog.Description>
+      <Dialog.Footer>Temp</Dialog.Footer>
     </>
   );
 }
+
+const styles = {
+  header: cssObj({
+    color: '$intentsBase12',
+  }),
+};
