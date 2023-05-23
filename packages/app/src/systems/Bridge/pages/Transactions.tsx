1c29edc70db4a0fb496f5e54066e229e034f8c3c
Matt Auer <mattauer@umich.edu>
2023-05-23 14:07:51 -0400

    Feat: basic page layout

---
+++ packages/app/src/systems/Bridge/pages/Transactions.tsx
@@ -0,0 +1,35 @@
+import { cssObj } from '@fuel-ui/css';
+import { Card, Text, Box, Pagination } from '@fuel-ui/react';
+
+export const Transactions = () => {
+  return (
+    <Card>
+      <Card.Header>
+        <Box.Flex justify="space-between" align="center" css={styles.header}>
+          <Text>Age</Text>
+          <Text>Direction</Text>
+          <Text>Asset</Text>
+          <Text>Status</Text>
+        </Box.Flex>
+      </Card.Header>
+      <Card.Footer>
+        <Box.Flex justify="center" align="center" css={styles.footer}>
+          <Pagination pagesCount={1}>
+            <Pagination.Prev />
+            <Pagination.Items />
+            <Pagination.Next />
+          </Pagination>
+        </Box.Flex>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+const styles = cssObj({
+  header: cssObj({
+    width: '100%',
+  }),
+  footer: cssObj({
+    width: '100%',
+  }),
+});
--- packages/app/src/systems/Bridge/routes.tsx
+++ packages/app/src/systems/Bridge/routes.tsx
@@ -1,9 +1,26 @@
 import { Route } from 'react-router-dom';
 
-import { BridgeHome } from './pages';
+import { BridgeHome, Transactions, Bridge } from './pages';
 
 import { Pages } from '~/types';
 
 export const bridgeRoutes = (
-  <Route path={Pages.bridge} element={<BridgeHome />} />
+  <>
+    <Route
+      path={Pages.bridge}
+      element={
+        <BridgeHome>
+          <Bridge />
+        </BridgeHome>
+      }
+    />
+    <Route
+      path={Pages.transactions}
+      element={
+        <BridgeHome>
+          <Transactions />
+        </BridgeHome>
+      }
+    />
+  </>
 );
