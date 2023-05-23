de2cfea0081af02dc7b82d456dbb009890ba32bd
Matt Auer <mattauer@umich.edu>
2023-05-23 17:20:08 -0400

    feat: skeleton of component

--- packages/app/src/systems/Bridge/pages/Transactions.tsx
+++ packages/app/src/systems/Bridge/pages/Transactions.tsx
@@ -1,13 +1,21 @@
 import { cssObj } from '@fuel-ui/css';
-import { Card, Text, Box, Pagination } from '@fuel-ui/react';
+import {
+  Card,
+  Text,
+  Box,
+  Pagination,
+  Image,
+  Icon,
+  FuelLogo,
+} from '@fuel-ui/react';
 import { useAccount, useMessages } from '@fuels-portal/sdk-react';
-import { Address } from 'fuels';
 
-import { FUEL_CHAIN } from '~/systems/Chains';
+import { FUEL_CHAIN, ethLogoSrc } from '~/systems/Chains';
 
 export const Transactions = () => {
   const { account } = useAccount();
-  const { messages } = useMessages(account, 5, FUEL_CHAIN.providerUrl);
+  const { messages } = useMessages(account, 10, FUEL_CHAIN.providerUrl);
+  // onst temp = messages.data.messages.nodes as Array;
 
   // TODO
   // 1.) fetch and filter a user's txs
@@ -19,6 +27,36 @@ export const Transactions = () => {
         <Text>Asset</Text>
         <Text>Status</Text>
       </Card.Header>
+      <Card.Body>
+        <Box.Stack>
+          <>
+            {messages &&
+              messages.map((message, index) => {
+                return (
+                  <Box.Flex
+                    key={`${index}-${message.nonce}`}
+                    justify="space-between"
+                  >
+                    <Text>5 days ago</Text>
+                    <Box.Flex css={styles.directionInfo}>
+                      <Image width={14} height={14} src={ethLogoSrc} />
+                      <Icon icon="ArrowNarrowRight" />
+                      <FuelLogo size={14} />
+                    </Box.Flex>
+                    <Box.Flex css={styles.txItem}>
+                      <Box.Flex css={styles.directionInfo}>
+                        <Image width={14} height={14} src={ethLogoSrc} />
+                        <Text css={styles.infoText}>1.500</Text>
+                        <Text css={styles.infoText}>ETH</Text>
+                      </Box.Flex>
+                    </Box.Flex>
+                    <Text>Settled</Text>
+                  </Box.Flex>
+                );
+              })}
+          </>
+        </Box.Stack>
+      </Card.Body>
       <Card.Footer>
         <Box.Flex justify="center" align="center" css={styles.footer}>
           <Pagination pagesCount={1}>
@@ -39,5 +77,21 @@ const styles = cssObj({
   }),
   footer: cssObj({
     width: '100%',
+  }),
+  txItem: cssObj({
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  }),
+  labelText: cssObj({
+    fontSize: '$xs',
+    color: '$intentsBase8',
+  }),
+  infoText: cssObj({
+    fontSize: '$xs',
+    color: '$intentsBase12',
+  }),
+  directionInfo: cssObj({
+    gap: '$1',
+    alignItems: 'center',
   }),
 });
