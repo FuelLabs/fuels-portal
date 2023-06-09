1632f63aaff8a8b56c8eca6068884c6a7a1ece06
Matt Auer <mattauer@umich.edu>
2023-06-09 10:18:36 -0400

    feat: add dialog skeleton

--- packages/app/src/systems/Chains/fuel/containers/TxFuelToEthDialog.tsx
+++ packages/app/src/systems/Chains/fuel/containers/TxFuelToEthDialog.tsx
@@ -1,23 +1,79 @@
-import { Dialog } from '@fuel-ui/react';
+import { cssObj } from '@fuel-ui/css';
+import { Dialog, Box, Text, Icon, IconButton } from '@fuel-ui/react';
+import { bn } from 'fuels';
 
+import { ETH_SYMBOL, ethLogoSrc, ETH_UNITS } from '../../eth';
 import { useTxFuelToEth } from '../hooks';
 
-import { BridgeSteps } from '~/systems/Bridge';
+import { BridgeSteps, BridgeTxOverview } from '~/systems/Bridge';
+import { shortAddress } from '~/systems/Core';
 import { useOverlay } from '~/systems/Overlay';
 
 export function TxFuelToEthDialog() {
   const { metadata } = useOverlay<{ txId: string }>();
-  const { steps } = useTxFuelToEth({
+  const { steps, handlers } = useTxFuelToEth({
     txId: metadata.txId,
   });
 
   return (
     <>
-      <Dialog.Close />
-      <Dialog.Heading>Transaction</Dialog.Heading>
+      <Dialog.Heading css={styles.dialogHeading}>
+        <Box.Flex justify="space-between" css={styles.dialogHeadingContainer}>
+          <Text fontSize="sm" color="intentsBase12">
+            Transaction: {shortAddress(metadata.txId)}
+          </Text>
+          <IconButton
+            data-action="closed"
+            variant="link"
+            icon={<Icon icon="CircleX" css={styles.dialogHeadingIcon} />}
+            aria-label="Close unlock window"
+            onPress={handlers.close}
+          />
+        </Box.Flex>
+      </Dialog.Heading>
       <Dialog.Description>
-        <BridgeSteps steps={steps} />
+        <Box.Stack gap="$2">
+          <Text css={styles.header}>Status</Text>
+          <BridgeSteps steps={steps} />
+          <Box css={styles.border} />
+          <BridgeTxOverview
+            transactionId={shortAddress(metadata.txId)}
+            date={new Date()}
+            isDeposit={true}
+            asset={{
+              assetSymbol: ETH_SYMBOL,
+              imageUrl: ethLogoSrc,
+              assetAmount: bn(0).format({
+                precision: 9,
+                units: ETH_UNITS,
+              }),
+            }}
+          />
+        </Box.Stack>
       </Dialog.Description>
     </>
   );
 }
+
+const styles = {
+  dialogHeading: cssObj({
+    borderBottom: '1px solid $intentsBase8',
+  }),
+  dialogHeadingContainer: cssObj({
+    paddingBottom: '$4',
+  }),
+  dialogHeadingIcon: cssObj({
+    color: '$intentsBase12 !important',
+  }),
+  header: cssObj({
+    color: '$intentsBase12',
+    fontSize: '$xs',
+  }),
+  border: cssObj({
+    my: '$4',
+    borderBottom: '1px solid $intentsBase8',
+  }),
+  actionButton: cssObj({
+    width: '100%',
+  }),
+};
