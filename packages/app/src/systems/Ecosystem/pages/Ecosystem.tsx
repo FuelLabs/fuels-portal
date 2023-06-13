f1e532697233fc41e02d96470dcb6e4fdc645116
tomide <ayotomiiide@gmail.com>
2023-06-13 21:29:38 +0100

    ft: setup ecosystem page

---
+++ packages/app/src/systems/Ecosystem/pages/Ecosystem.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@fuel-ui/react';
+
+import { Layout } from '~/systems/Core';
+
+export function Ecosystem() {
+  return (
+    <Layout>
+      <Text>Ecosystem Page</Text>
+    </Layout>
+  );
+}
