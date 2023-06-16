4ac8944cc76a93685f6dd4d38587d401469ee0bf
Matt Auer <mattauer@umich.edu>
2023-06-16 14:38:15 -0400

    test: part of e2e

--- packages/app/playwright/e2e/Bridge.test.ts
+++ packages/app/playwright/e2e/Bridge.test.ts
@@ -1,4 +1,4 @@
-import type { Browser, Page } from '@playwright/test';
+import type { Browser, BrowserContext, Page } from '@playwright/test';
 import { chromium } from '@playwright/test';
 import * as metamask from '@synthetixio/synpress/commands/metamask';
 
@@ -6,20 +6,87 @@ import { getByAriaLabel, hasText, visit, getButtonByText } from '../commons';
 
 import { test } from './fixtures';
 
+const MNEMONIC =
+  'demand fashion unaware upgrade upon heart bright august panel kangaroo want gaze';
+const WALLET_PASSWORD = '$123Ran123Dom123!';
+
+async function walletSetup(
+  context: BrowserContext,
+  fuelExtensionId: string,
+  page: Page
+) {
+  // const appPage = await context.newPage();
+  // await appPage.goto('/');
+
+  // const hasFuel = await appPage.evaluate(() => {
+  //   return typeof window.fuel === 'object';
+  // });
+  // expect(hasFuel).toBeTruthy();
+
+  // Wallet Setup
+  // const walletPage = await context.newPage();
+  await page.goto(`chrome-extension://${fuelExtensionId}/popup.html`);
+
+  const signupPage = await context.waitForEvent('page', {
+    predicate: (page) => page.url().includes('sign-up'),
+  });
+  expect(signupPage.url()).toContain('sign-up');
+
+  const button = signupPage.locator('h3').getByText('Import seed phrase');
+  await button.click();
+
+  // Agree to T&S
+  await signupPage.getByRole('checkbox').click();
+  const toSeedPhrase = getButtonByText(signupPage, 'Next: Seed Phrase');
+  await toSeedPhrase.click();
+
+  // Copy and paste seed phrase
+  /** Copy words to clipboard area */
+  await signupPage.evaluate(`navigator.clipboard.writeText('${MNEMONIC}')`);
+  const pasteButton = signupPage.locator('button').getByText('Paste');
+  await pasteButton.click();
+  const toPassword = signupPage
+    .locator('button')
+    .getByText('Next: Your password');
+  await toPassword.click();
+
+  // Enter password
+  const enterPassword = signupPage.locator(`[aria-label="Your Password"]`);
+  await enterPassword.type(WALLET_PASSWORD);
+  // Confirm password
+  const confirmPassword = signupPage.locator(`[aria-label="Confirm Password"]`);
+  await confirmPassword.type(WALLET_PASSWORD);
+  const toFinish = getButtonByText(signupPage, 'Next: Finish set-up');
+  await toFinish.click();
+}
+
+async function walletApprove(context: BrowserContext) {
+  let approvePage = context.pages().find((p) => p.url().includes('/popup'));
+  if (!approvePage) {
+    approvePage = await context.waitForEvent('page', {
+      predicate: (page) => page.url().includes('/popup'),
+    });
+  }
+
+  const nextButton = approvePage.locator('button').getByText('Next');
+  await nextButton.click();
+  const connectButton = approvePage.locator('button').getByText('Connect');
+  await connectButton.click();
+}
+
 test.describe('Bridge', () => {
   // let browser: Browser;
   // let page: Page;
+
+  test.beforeAll(async ({ context, extensionId, page }) => {
+    await walletSetup(context, extensionId, page);
+  });
 
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
   });
 
-  // test.beforeAll(async () => {
-  //   browser = await chromium.launch();
-  //   page = await browser.newPage();
-  // });
-
-  test('e2e', async ({ page }) => {
+  test('e2e', async ({ context, page }) => {
     // Go to the bridge page
     const goToBridge = getByAriaLabel(page, 'Bridge');
     await goToBridge.click();
@@ -29,11 +96,27 @@ test.describe('Bridge', () => {
     await connectKitButton.click();
     const metamaskConnect = getButtonByText(page, 'Metamask');
     await metamaskConnect.click();
+    // sometimes it goes too quick
+    await page.waitForTimeout(3000);
     await metamask.acceptAccess();
-  });
 
-  // test('Should render Fuel Native Bridge', async () => {
-  //   await visit(page, '/bridge');
-  //   await hasText(page, /Fuel Native Bridge/i);
-  // });
+    // Connect fuel
+    const connectFuel = getByAriaLabel(page, 'To Connect wallet');
+    await connectFuel.click();
+    await walletApprove(context);
+
+    const depositInput = page.locator('input');
+    await depositInput.fill('1');
+    const depositButton = getButtonByText(page, 'Bridge asset');
+    await depositButton.click();
+    await metamask.confirmTransaction();
+
+    // check the popup is correct?
+    const closeEthPopup = getByAriaLabel(page, 'Close unlock window');
+    await closeEthPopup.click();
+
+    // Go to transaction page
+    const transactionList = page.locator('a').getByText('Transactions');
+    await transactionList.click();
+  });
 });
--- packages/app/playwright/e2e/fixtures.ts
+++ packages/app/playwright/e2e/fixtures.ts
@@ -4,28 +4,55 @@ import type { BrowserContext } from '@playwright/test';
 import { chromium, test as base } from '@playwright/test';
 import { initialSetup } from '@synthetixio/synpress/commands/metamask';
 import { prepareMetamask } from '@synthetixio/synpress/helpers';
-// import admZip from 'adm-zip';
-// import * as fs from 'fs';
-// import https from 'https';
-// import path from 'path';
+import admZip from 'adm-zip';
+import * as fs from 'fs';
+import https from 'https';
+import path from 'path';
 
-// const pathToExtension = path.join(__dirname, './dist-crx');
+const pathToExtension = path.join(__dirname, './dist-crx');
 
 export const test = base.extend<{
-  extensionId: string;
   context: BrowserContext;
+  extensionId: string;
 }>({
   context: async ({}, use) => {
     // required for synpress
     global.expect = expect;
+
+    const extensionUrl = 'https://wallet.fuel.network/app/fuel-wallet.zip';
+
+    const zipFile = './packages/app/playwright/e2e/fuel-wallet.zip';
+    const zipFileStream = fs.createWriteStream(zipFile);
+    // TODO fetch the exact version of wallet to avoid breaking ci
+    const zipPromise = new Promise((resolve, reject) => {
+      https
+        .get(extensionUrl, (res) => {
+          res.pipe(zipFileStream);
+          // after download completed close filestream
+          zipFileStream.on('finish', async () => {
+            zipFileStream.close();
+            console.log('Download Completed extracting zip...');
+            const zip = new admZip(zipFile); // eslint-disable-line new-cap
+            zip.extractAllTo('./packages/app/playwright/e2e/dist-crx', true);
+            console.log('zip extracted');
+            resolve(true);
+          });
+        })
+        .on('error', (error) => {
+          console.log('error: ', error);
+          reject(error);
+        });
+    });
+    await zipPromise;
+
     // download metamask
     const metamaskPath = await prepareMetamask(
       process.env.META_MASK_VERSION || '10.25.0'
     );
     // prepare browser args
     const browserArgs = [
-      `--disable-extensions-except=${metamaskPath}`,
-      `--load-extension=${metamaskPath}`,
+      `--disable-extensions-except=${metamaskPath},${pathToExtension}`,
+      `--load-extension=${metamaskPath},${pathToExtension}`,
       '--remote-debugging-port=9222',
     ];
     if (process.env.CI) {
@@ -45,64 +72,20 @@ export const test = base.extend<{
     await initialSetup(chromium, {
       secretWordsOrPrivateKey:
         'test test test test test test test test test test test junk',
-      network: 'sepolia',
+      rpcUrl: 'http://localhost:8080',
+      network: 'localhost',
       password: 'Tester@1234',
       enableAdvancedSettings: true,
     });
     await use(context);
     await context.close();
   },
-  // extensionId: async ({ context }, use) => {
-  //   let [background] = context.serviceWorkers();
-  //   if (!background) background = await context.waitForEvent('serviceworker');
-  //   const extensionId = background.url().split('/')[2];
-  //   await use(extensionId);
-  // },
+  extensionId: async ({ context }, use) => {
+    let [background] = context.serviceWorkers();
+    if (!background) background = await context.waitForEvent('serviceworker');
+    const extensionId = background.url().split('/')[2];
+    await use(extensionId);
+  },
 });
 
 export const expect = test.expect;
-
-// let context: BrowserContext;
-
-// test.beforeAll(async () => {
-//   const extensionUrl = 'https://wallet.fuel.network/app/fuel-wallet.zip';
-
-//   const zipFile = './packages/app/playwright/e2e/fuel-wallet.zip';
-//   const zipFileStream = fs.createWriteStream(zipFile);
-//   // TODO fetch the exact version of wallet to avoid breaking ci
-//   const zipPromise = new Promise((resolve, reject) => {
-//     https
-//       .get(extensionUrl, (res) => {
-//         res.pipe(zipFileStream);
-//         // after download completed close filestream
-//         zipFileStream.on('finish', async () => {
-//           zipFileStream.close();
-//           console.log('Download Completed extracting zip...');
-//           const zip = new admZip(zipFile); // eslint-disable-line new-cap
-//           zip.extractAllTo('./packages/app/playwright/e2e/dist-crx', true);
-//           console.log('zip extracted');
-//           context = await chromium.launchPersistentContext('', {
-//             headless: false,
-//             args: [
-//               `--disable-extensions-except=${pathToExtension}`,
-//               `--load-extension=${pathToExtension},`,
-//             ],
-//           });
-//           resolve(context);
-//         });
-//       })
-//       .on('error', (error) => {
-//         console.log('error: ', error);
-//         reject(error);
-//       });
-//   });
-//   await zipPromise;
-// });
-
-// test.use({
-//   context: ({}, use) => {
-//     use(context);
-//   },
-// });
-
-// export const expect = test.expect;
