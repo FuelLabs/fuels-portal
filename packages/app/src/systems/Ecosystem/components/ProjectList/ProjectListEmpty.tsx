608d8ff9eaa1b700724978b629cd49991c706044
tomide <ayotomiiide@gmail.com>
2023-06-21 15:59:11 +0100

    ft: create project list empty component

--- packages/app/src/systems/Ecosystem/components/ProjectList/ProjectListEmpty.tsx
+++ packages/app/src/systems/Ecosystem/components/ProjectList/ProjectListEmpty.tsx
@@ -1,14 +1,7 @@
-import { Box, ButtonLink, Text } from '@fuel-ui/react';
+import { Box, Text } from '@fuel-ui/react';
 
-type ProjectListEmptyProps = {
-  onShowAll: () => void;
-};
-
-export const ProjectListEmpty = ({ onShowAll }: ProjectListEmptyProps) => (
+export const ProjectListEmpty = () => (
   <Box.Flex justify="center">
     <Text> There are no projects that match your search. </Text>
-    {onShowAll && (
-      <ButtonLink onClick={() => onShowAll?.()}>Show all projects.</ButtonLink>
-    )}
   </Box.Flex>
 );
