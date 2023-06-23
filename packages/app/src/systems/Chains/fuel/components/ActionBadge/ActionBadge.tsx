a491d3bf99f250609ed2f87772468f9d33c41f6d
Matt <54373384+matt-user@users.noreply.github.com>
2023-06-23 09:44:41 -0400

    feat: add action badge (#55)
    * feat: add action badge
    
    * chore: fix lint'
    
    * fix: audit
    
    * fix: audit
    
    * test: remove failing test as it currently does nothing anyway

---
+++ packages/app/src/systems/Chains/fuel/components/ActionBadge/ActionBadge.tsx
@@ -0,0 +1,19 @@
+import { cssObj } from '@fuel-ui/css';
+import { Badge } from '@fuel-ui/react';
+
+export const ActionBadge = () => {
+  return (
+    <Badge css={styles.actionBadge} intent="error">
+      Action Required
+    </Badge>
+  );
+};
+
+const styles = {
+  actionBadge: cssObj({
+    fontSize: '$xs',
+    lineHeight: 1,
+    padding: '$1',
+    textTransform: 'none',
+  }),
+};
