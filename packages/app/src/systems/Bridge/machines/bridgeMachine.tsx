eb0fb63c8d76fc5909ad9f3258a832434edc025a
Matt Auer <mattauer@umich.edu>
2023-07-06 09:54:46 -0400

    fix: input amount issue

--- packages/app/src/systems/Bridge/machines/bridgeMachine.tsx
+++ packages/app/src/systems/Bridge/machines/bridgeMachine.tsx
@@ -110,7 +110,9 @@ export const bridgeMachine = createMachine(
         toNetwork: ev.input.toNetwork,
       })),
       assignAssetAmount: assign({
-        assetAmount: (_, ev) => ev.input.assetAmount,
+        assetAmount: (_, ev) => {
+          return ev.input.assetAmount?.eq(0) ? undefined : ev.input.assetAmount;
+        },
       }),
       clearAssetAmmount: assign({
         assetAmount: undefined,
