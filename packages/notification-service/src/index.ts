e04547c8b86bbe43e50e8e0109019fea937941e3
Matt Auer <mattauer@umich.edu>
2023-08-14 16:50:36 -0400

    feat: basic tracking

--- packages/notification-service/src/index.ts
+++ packages/notification-service/src/index.ts
@@ -1,14 +1,44 @@
+import * as dotenv from 'dotenv';
 import type { Request, Response } from 'express';
 import express from 'express';
+import { Provider, Address } from 'fuels';
+
+import { getGraphqlClient } from './utils/graphql';
+
+dotenv.config();
 
 const notificationServer = express();
 const port = 3005;
+const fuelTestAddress = new Address(
+  'fuel1jraekwq3e2f8fn7ldypjvvy3zqgzgf99supvcylzhxqvrr0mx3nqdnzpvw'
+);
+
+const fuelProvider = new Provider(process.env.FUEL_PROVIDER_URL || '');
+
+const getWithdrawTransactions = async (
+  address: string,
+  providerUrl: string,
+  numberOfTransactions = 1000
+) => {
+  const { transactionsByOwner } = await getGraphqlClient(
+    providerUrl
+  ).AddressTransactions({
+    owner: address,
+    first: numberOfTransactions,
+  });
+  return transactionsByOwner;
+};
 
 notificationServer.get('/', (req: Request, res: Response) => {
   res.send('hello');
 });
 
-notificationServer.listen(port, () => {
+notificationServer.listen(port, async () => {
   // eslint-disable-next-line no-console
   console.log(`Notification server running at http://localhost:${port}`);
+  const transactions = await getWithdrawTransactions(
+    fuelTestAddress.toB256(),
+    fuelProvider.url
+  );
+  console.log(`transactions`, JSON.stringify(transactions, null, 4));
 });
