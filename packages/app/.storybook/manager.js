e0fe1ad6a51c67aa8d5f77802fd031a2d2fb8db0
Pedro Nauck <pedronauck@gmail.com>
2023-08-21 18:46:46 -0300

    fix: icons

--- packages/app/.storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/manager-api';
-import fuelTheme from './theme';
-
-addons.setConfig({
-  theme: fuelTheme,
-});
--- packages/app/src/systems/Settings/providers/FuelUiProvider.tsx
+++ packages/app/src/systems/Settings/providers/FuelUiProvider.tsx
@@ -1,5 +1,23 @@
-import { ThemeProvider } from '@fuel-ui/react';
+import {
+  darkTheme,
+  lightTheme,
+  loadIcons,
+  setFuelThemes,
+  ThemeProvider,
+} from '@fuel-ui/react';
 import type { PropsWithChildren } from 'react';
+
+// eslint-disable-next-line import/no-absolute-path
+import icons from '/icons/sprite.svg';
+
+loadIcons(icons);
+setFuelThemes({
+  initial: 'light',
+  themes: {
+    dark: darkTheme,
+    light: lightTheme,
+  },
+});
 
 export function FuelUiProvider({ children }: PropsWithChildren) {
   return <ThemeProvider>{children}</ThemeProvider>;
