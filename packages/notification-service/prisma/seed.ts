1f26b8379553cd00783013b8fd4e5e5626560e64
Matt Auer <mattauer@umich.edu>
2023-09-06 09:37:21 -0500

    feat: clear db before seed

--- packages/notification-service/prisma/seed.ts
+++ packages/notification-service/prisma/seed.ts
@@ -5,15 +5,27 @@ const prisma = new PrismaClient();
 
 export const fuelTestEmail = 'test@test.com';
 export const fuelTestAddress = new Address(
-  'fuel1jraekwq3e2f8fn7ldypjvvy3zqgzgf99supvcylzhxqvrr0mx3nqdnzpvw'
+  'fuel18e7amfxs60urq7h97xhdsa3rnykpcn0valkxsjfkjcrh2xqmyvpq4ay9jn'
 ).toB256();
 
 async function main() {
+  const deleteUsers = prisma.user.deleteMany();
+  const deleteAddresses = prisma.address.deleteMany();
+  const deleteTransactions = prisma.transaction.deleteMany();
+  await prisma.$transaction([deleteUsers, deleteAddresses, deleteTransactions]);
+
   await prisma.user.upsert({
     where: {
       email: fuelTestEmail,
     },
-    update: {},
+    update: {
+      email: fuelTestEmail,
+      addresses: {
+        create: {
+          address: fuelTestAddress,
+        },
+      },
+    },
     create: {
       email: fuelTestEmail,
       addresses: {
