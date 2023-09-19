0b3cb68eda22f55465d0220f3e422e9bf2cc6dd8
Matt Auer <mattauer@umich.edu>
2023-09-19 13:54:01 -0500

    refactor: changes to db and service

--- packages/notification-service/src/services/mailService.ts
+++ packages/notification-service/src/services/mailService.ts
@@ -1,46 +1,52 @@
 import nodemailer from 'nodemailer';
 
 export default class MailService {
-    private static instance: MailService;
-    private transporter: nodemailer.Transporter;
+  private transporter: nodemailer.Transporter;
 
-    private constructor() {}
+  private static instance: MailService;
 
-    static getInstance() {
-        if (!MailService.instance) {
-            MailService.instance = new MailService();
-        }
-        return MailService.instance;
+  private constructor() {
+    this.createLocalConnection();
+  }
+
+  static async getInstance() {
+    if (!MailService.instance) {
+      MailService.instance = new MailService();
     }
-
-    async createLocalConnection() {
-        const account = await nodemailer.createTestAccount();
-        this.transporter = nodemailer.createTransport({
-            host: account.smtp.host,
-            port: account.smtp.port,
-            secure: account.smtp.secure,
-            auth: {
-                user: account.user,
-                pass: account.pass,
-            }
-        });
+    if (!this.instance.transporter) {
+      await this.instance.createLocalConnection();
     }
+    return MailService.instance;
+  }
 
-    async sendMail(options: {
-        from?: string;
-        to: string;
-        subject: string;
-        text: string;
-        html: string;
-    }) {
-        const info = await this.transporter.sendMail({
-            from: `"Fred Foo" ${options.from}`,
-            to: options.to,
-            subject: options.subject,
-            text: options.text,
-            html: options.html,
-        })
-        console.log(`ethereal url: ${nodemailer.getTestMessageUrl(info)}`);
-        return info;
-    }
+  async createLocalConnection() {
+    const account = await nodemailer.createTestAccount();
+    this.transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+  }
+
+  async sendMail(options: {
+    from?: string;
+    to: string;
+    subject: string;
+    text: string;
+    html: string;
+  }) {
+    const info = await this.transporter.sendMail({
+      from: `"Fred Foo" ${options.from}`,
+      to: options.to,
+      subject: options.subject,
+      text: options.text,
+      html: options.html,
+    });
+    console.log(`ethereal url: ${nodemailer.getTestMessageUrl(info)}`);
+    return info;
+  }
 }
--- packages/notification-service/src/utils/handleNewEthBlock.ts
+++ packages/notification-service/src/utils/handleNewEthBlock.ts
@@ -8,29 +8,16 @@ import {
   ReceiptType,
 } from 'fuels';
 import type { PublicClient } from 'viem';
-import { getContract } from 'viem';
 import { FUEL_CHAIN_STATE } from '~/contracts/FuelChainState';
 import { FUEL_MESSAGE_PORTAL } from '~/contracts/FuelMessagePortal';
-import MailService from "../services/mailService";
 
-// import { getWithdrawTransactions } from './withdrawTransactions';
+import MailService from '../services/mailService';
 
 export const handleNewEthBlock = async (
   prisma: PrismaClient,
   fuelProviderUrl: string,
   ethPublicClient: PublicClient
 ) => {
-  // Create the instance of the fuelChainState contract from ETH
-  //const fuelChainState = getContract({
-  //     abi: FUEL_CHAIN_STATE.abi,
-  //     address: process.env.ETH_FUEL_CHAIN_STATE as `0x${string}`,
-  //     publicClient: ethPublicClient,
-  //   });
-  //   const fuelMessagePortal = getContract({
-  //     abi: FUEL_MESSAGE_PORTAL.abi,
-  //     address: process.env.ETH_FUEL_MESSAGE_PORTAL as `0x${string}`,
-  //     publicClient: ethPublicClient,
-  //   });
   const abiFuelChainState = FUEL_CHAIN_STATE.abi.find(
     ({ name, type }) => name === 'CommitSubmitted' && type === 'event'
   );
@@ -43,23 +30,16 @@ export const handleNewEthBlock = async (
       name: 'CommitSubmitted',
       inputs: abiFuelChainState?.inputs || [],
     },
-    fromBlock: 'earliest' as any, // eslint-disable @typescript-eslint/no-explicit-any
+    fromBlock: 'earliest',
   });
 
-  // Grab the 3rd to last
   const last5Logs = logs.slice(-5); // Remove the slice here to grab all the logs
   last5Logs.map(async (lastLog) => {
-    const { blockHash } = lastLog.args as any;
+    const { blockHash } = lastLog.args as any; // eslint-disable-line @typescript-eslint/no-explicit-any
 
     const fuelProvider = new Provider(fuelProviderUrl);
     try {
-      // Check if the block is finalized or not
       const block = await fuelProvider.getBlock(blockHash);
-      //console.log(block.height.toString());
-      //   const isFinalized = await fuelChainState.read.finalized([
-      //     blockHash,
-      //     block.height.toString(),
-      //   ]);
 
       const addresses = await prisma.address.findMany({
         include: {
@@ -68,9 +48,9 @@ export const handleNewEthBlock = async (
       });
 
       addresses.forEach(async ({ address }) => {
-        // Grap the owner
+        console.log(`address`, address);
         const owner = Address.fromString(address).toB256();
-        // Grap all the withdraws from the Owner
+
         const transactionsByOwner = await getTransactionsSummaries({
           provider: fuelProvider,
           filters: {
@@ -78,25 +58,25 @@ export const handleNewEthBlock = async (
             first: 1000,
           },
         });
-        // Filter all transaction that have a withdrw to ETH
+
+        // Filter all transaction that have a withdraw to ETH
         const withdrawTransactions = transactionsByOwner.transactions.filter(
           (t) => {
-            return t.operations.find((o) => o.to.chain === ChainName.ethereum);
+            return t.operations.find((o) => o.to?.chain === ChainName.ethereum);
           }
         );
 
         // Get the blocks that need to be notified
         const withdrawsWithBlocks = await Promise.all(
           withdrawTransactions.map(async (tranasction) => {
-            const blockOfTransaction = await fuelProvider.getBlock(
-              tranasction.blockId
-            );
             const messageReceipt = tranasction.receipts.find(
               (r) => r.type === ReceiptType.MessageOut
             ) as ReceiptMessageOut;
+
             const abiMessageRelayed = FUEL_MESSAGE_PORTAL.abi.find(
               ({ name, type }) => name === 'MessageRelayed' && type === 'event'
             );
+
             const logs = await ethPublicClient.getLogs({
               address: process.env.ETH_FUEL_MESSAGE_PORTAL as `0x${string}`,
               event: {
@@ -108,64 +88,80 @@ export const handleNewEthBlock = async (
                 messageId: messageReceipt?.messageId as `0x${string}`,
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
               } as any,
-              fromBlock: 'earliest' as any,
+              fromBlock: 'earliest',
             });
+
             const isAlreadyRelayed = !!logs[0];
+
+            const isReady = tranasction.blockId
+              ? (await fuelProvider.getBlock(tranasction.blockId))?.height.lte(
+                  block?.height || 0
+                )
+              : false;
 
             return {
               tranasction,
               block,
               isAlreadyRelayed,
-              isReady: blockOfTransaction.height.lte(block.height),
+              isReady,
             };
           })
         );
-        //
 
         withdrawsWithBlocks.forEach(async (w) => {
           // Message is already sent
           // Check if the email was already sent
           let dbTransaction = await prisma.transaction.findUnique({
             where: {
-                transactionId: w.tranasction.id,
+              transactionId: w.tranasction.id,
             },
             include: {
-                address: {
-                    include: {
-                        withdrawer: true,
-                    }
-                }
-            }
+              address: {
+                include: {
+                  withdrawer: true,
+                },
+              },
+            },
           });
           if (!dbTransaction && w.tranasction.id && w.tranasction.status) {
             dbTransaction = await prisma.transaction.create({
-                data: {
-                    transactionId: w.tranasction.id,
-                    status: w.tranasction.status,
-                    address: {
-                        connect: {
-                            address: owner,
-                        }
-                    },
-                    blockHeight: w.block?.height.toNumber(),
-                    messageId: "",
+              data: {
+                transactionId: w.tranasction.id,
+                status: w.tranasction.status,
+                address: {
+                  connect: {
+                    address: owner,
+                  },
                 },
-                include: {
-                    address: {
-                        include: {
-                            withdrawer: true,
-                        }
-                    }
-                }
+                blockHeight: w.block?.height.toNumber(),
+              },
+              include: {
+                address: {
+                  include: {
+                    withdrawer: true,
+                  },
+                },
+              },
             });
           }
-          if (w.isReady && !w.isAlreadyRelayed && dbTransaction && !dbTransaction.emailSent) {
-            const mailService = MailService.getInstance();
-            await mailService.sendMail({ from: "matt.auer@fuel.sh", to: dbTransaction.address.withdrawer.email, subject: "test", text: "test", html: "<b>Test<b>"});
+          if (
+            w.isReady &&
+            !w.isAlreadyRelayed &&
+            dbTransaction &&
+            !dbTransaction.emailSent
+          ) {
+            const mailService = await MailService.getInstance();
+            await mailService.sendMail({
+              from: 'matt.auer@fuel.sh',
+              to: dbTransaction.address.withdrawer.email,
+              subject: 'Withdraw Notification',
+              text: `Your transaction ${w.tranasction.id} is ready for withdrawal`,
+              html: `<p>Your transaction ${w.tranasction.id} is ready for withdrawal<p>`,
+            });
             // Update the message is sent
             await prisma.transaction.update({
-                where: { transactionId: dbTransaction.transactionId },
-                data: { emailSent: true },
+              where: { transactionId: dbTransaction.transactionId },
+              data: { emailSent: true },
             });
           }
         });
