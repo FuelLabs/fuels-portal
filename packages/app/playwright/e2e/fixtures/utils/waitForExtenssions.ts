37c847695bf2f8d8b72d9542034523cb9824e237
Luiz Estácio <luizstacio@gmail.com>
2023-09-19 23:02:17 -0300

    chore: improve fixtures to better redability

---
+++ packages/app/playwright/e2e/fixtures/utils/waitForExtenssions.ts
@@ -0,0 +1,37 @@
+import type { BrowserContext } from '@playwright/test';
+import { setTimeout } from 'timers/promises';
+
+export async function waitForExtensions(
+  context: BrowserContext,
+  extensions: Record<
+    string,
+    {
+      id: string;
+      version: string;
+    }
+  >,
+  attempts: number = 0
+) {
+  console.log('extenssions', extensions);
+  const pages = await context.pages();
+  const hasMetamask = pages.find((page) => {
+    return page.url().includes(extensions['metamask']?.id);
+  });
+  const hasFuelWallet = pages.find((page) => {
+    return page.url().includes(extensions['fuel wallet']?.id);
+  });
+  console.log(
+    'page urls',
+    pages.map((p) => p.url())
+  );
+  console.log('Waiting for the pages');
+  if (!hasMetamask || !hasFuelWallet) {
+    if (attempts > 5) {
+      throw new Error('Too many attempts to wait for the extensions');
+    }
+    await setTimeout(3000);
+    return waitForExtensions(context, extensions, attempts + 1);
+  }
+  console.log('Pages found!');
+  return true;
+}
