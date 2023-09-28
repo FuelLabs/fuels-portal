0bdf1fc1dcfa4099ed8673822b05e0b93cf5e1b2
LuizAsFight <felipebolsonigomes@gmail.com>
2023-09-28 01:33:24 -0300

    chore

--- packages/app/load.envs.js
+++ packages/app/load.envs.js
@@ -32,9 +32,9 @@ function getBridgeTokenContracts() {
 
   if (process.env.VITE_ETH_CHAIN === 'sepolia') {
     return {
-      ETH_ERC20: '0x39B7136Ad0108B2F4E5d1bc360efc1c62f4B0C5b',
+      ETH_ERC20: '0xC6387efAD0F184a90B34f397C3d6Fd63135ef790',
       FUEL_TokenContract:
-        '0xc95e66eb3dc38afca1aa21ea6c30663cab39e468a0ebea4bd87252fe73ec8b5f',
+        '0x5ed7bee1e80a64e5163a1c93ff8a2bb806fdff3b7e97808f275c2ba7e3faa2cd',
     };
   }
   return {};
