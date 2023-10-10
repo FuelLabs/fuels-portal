c30f363953d17c15254b97c3f38bac782a64a924
Luiz Gomes - LuizAsFight.eth <8636507+LuizAsFight@users.noreply.github.com>
2023-10-10 19:38:57 -0300

    fix: add pagination to bridge tx list (#138)

--- packages/app/src/systems/Bridge/pages/BridgeTxList.tsx
+++ packages/app/src/systems/Bridge/pages/BridgeTxList.tsx
@@ -1,5 +1,5 @@
 import { cssObj } from '@fuel-ui/css';
-import { CardList } from '@fuel-ui/react';
+import { Button, CardList } from '@fuel-ui/react';
 import {
   useFuelAccountConnection,
   TxListItemEthToFuel,
@@ -16,13 +16,15 @@ import {
 import { useBridgeTxs } from '../hooks';
 
 export const BridgeTxList = () => {
-  const { isConnecting, handlers } = useFuelAccountConnection();
+  const { isConnecting, handlers: fuelHandlers } = useFuelAccountConnection();
   const {
+    handlers,
     bridgeTxs,
     isLoading,
     shouldShowNotConnected,
     shouldShowEmpty,
     shouldShowList,
+    hasMorePages,
   } = useBridgeTxs();
 
   return (
@@ -31,39 +33,54 @@ export const BridgeTxList = () => {
       {shouldShowNotConnected && (
         <BridgeTxListNotConnected
           isConnecting={isConnecting}
-          onClick={handlers.connect}
+          onClick={fuelHandlers.connect}
         />
       )}
       {shouldShowEmpty && <BridgeListEmpty />}
       {shouldShowList && (
-        <CardList isClickable css={styles.cardList}>
-          {bridgeTxs?.map((txDatum, index) => {
-            if (
-              isEthChain(txDatum.fromNetwork) &&
-              isFuelChain(txDatum.toNetwork)
-            ) {
-              return (
-                <TxListItemEthToFuel
-                  key={`${index}-${txDatum.txHash}`}
-                  txHash={txDatum.txHash || ''}
-                />
-              );
-            }
-            if (
-              isFuelChain(txDatum.fromNetwork) &&
-              isEthChain(txDatum.toNetwork)
-            ) {
-              return (
-                <TxListItemFuelToEth
-                  key={`${index}-${txDatum.txHash}`}
-                  txHash={txDatum.txHash || ''}
-                />
-              );
-            }
+        <>
+          <CardList isClickable css={styles.cardList}>
+            {bridgeTxs?.map((txDatum, index) => {
+              if (
+                isEthChain(txDatum.fromNetwork) &&
+                isFuelChain(txDatum.toNetwork)
+              ) {
+                return (
+                  <TxListItemEthToFuel
+                    key={`${index}-${txDatum.txHash}`}
+                    txHash={txDatum.txHash || ''}
+                  />
+                );
+              }
+              if (
+                isFuelChain(txDatum.fromNetwork) &&
+                isEthChain(txDatum.toNetwork)
+              ) {
+                return (
+                  <TxListItemFuelToEth
+                    key={`${index}-${txDatum.txHash}`}
+                    txHash={txDatum.txHash || ''}
+                  />
+                );
+              }
 
-            return null;
-          })}
-        </CardList>
+              return null;
+            })}
+          </CardList>
+          {hasMorePages && (
+            <Button
+              variant="link"
+              onPress={handlers.showMore}
+              size="sm"
+              intent="info"
+              css={styles.buttonShowMore}
+              rightIcon="ChevronDown"
+              iconSize={13}
+            >
+              Show more
+            </Button>
+          )}
+        </>
       )}
     </>
   );
@@ -77,5 +94,8 @@ const styles = {
     ':hover': {
       backgroundColor: '$intentsBase3',
     },
+  }),
+  buttonShowMore: cssObj({
+    mt: '$2',
   }),
 };
