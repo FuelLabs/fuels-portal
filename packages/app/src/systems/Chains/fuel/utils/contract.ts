686ef5565a2023ddfaf94f5ddebbae4ef0523234
Luiz Gomes - LuizAsFight.eth <8636507+LuizAsFight@users.noreply.github.com>
2023-10-21 16:47:58 -0300

    feat: update to beta-4 (#171)

--- packages/app/src/systems/Chains/fuel/utils/contract.ts
+++ packages/app/src/systems/Chains/fuel/utils/contract.ts
@@ -1,8 +1,11 @@
-import { ZeroBytes32, hash, concat } from 'fuels';
+import { concatBytes } from '@fuel-ts/utils';
+import { ZeroBytes32, arrayify, hash } from 'fuels';
 
 export function getContractTokenId(
   contractId: `0x${string}`,
   subId = ZeroBytes32
 ) {
-  return hash(concat([contractId, subId]));
+  const byteContract = arrayify(contractId);
+  const byteSubId = arrayify(subId);
+  return hash(concatBytes([byteContract, byteSubId]));
 }
