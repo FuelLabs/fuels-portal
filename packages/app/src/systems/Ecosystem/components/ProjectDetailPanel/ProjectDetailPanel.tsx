78ebb8df46372a6ecd667907c903032e58fc8526
Ruben Amar <43066067+Rubyt0x@users.noreply.github.com>
2023-10-31 11:27:51 +0100

    docs: closes the side panel when clicking outside

--- packages/app/src/systems/Ecosystem/components/ProjectDetailPanel/ProjectDetailPanel.tsx
+++ packages/app/src/systems/Ecosystem/components/ProjectDetailPanel/ProjectDetailPanel.tsx
@@ -2,6 +2,7 @@
 
 import { Box, Image, Link } from '@fuel-ui/react';
 import type { FC } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import type { Project } from '../../types';
 
@@ -14,8 +15,29 @@ const ProjectDetailPanel: FC<ProjectDetailPanelProps> = ({
   project,
   onClose,
 }) => {
+  const panelRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        panelRef.current &&
+        !panelRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    }
+
+    // Attach the click event handler
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Remove event listener on cleanup
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
     <div
+      ref={panelRef} // Added ref here
       style={{
         position: 'fixed',
         right: 0,
@@ -23,14 +45,25 @@ const ProjectDetailPanel: FC<ProjectDetailPanelProps> = ({
         height: '100%',
         width: '300px',
         backgroundColor: '#fff',
-        zIndex: 9999,
+        boxShadow: '-2px 0px 5px rgba(0, 0, 0, 0.1)',
+        padding: '20px',
+        overflowY: 'auto',
+        zIndex: 1000, // adjust as needed
       }}
     >
-      <button onClick={onClose}>Close</button>
-      <Image src={project.image || 'default_image_url'} alt="Profile" />
-      <h2>{project.name}</h2>
-      <p>{project.description}</p>
-      <Box>
+      <button onClick={onClose} style={{ marginBottom: '20px' }}>
+        Close
+      </button>
+      <Image
+        src={project.image || 'default_image_url'}
+        alt="Profile"
+        style={{ width: '100%', borderRadius: '10px', marginBottom: '20px' }}
+      />
+      <h2 style={{ fontSize: '24px', marginBottom: '10px' }}>{project.name}</h2>
+      <p style={{ fontSize: '16px', marginBottom: '20px' }}>
+        {project.description}
+      </p>
+      <Box style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
         {project.twitter && <Link href={project.twitter}>Twitter</Link>}
         {project.github && <Link href={project.github}>GitHub</Link>}
         {project.discord && <Link href={project.discord}>Discord</Link>}
