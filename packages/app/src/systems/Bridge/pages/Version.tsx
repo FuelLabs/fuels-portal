93688b26e4dbe49bb82d92de143636323d3eccbd
LuizAsFight <felipebolsonigomes@gmail.com>
2023-12-04 10:33:43 -0300

    feat: improve versioning

--- packages/app/src/systems/Bridge/pages/Version.tsx
+++ packages/app/src/systems/Bridge/pages/Version.tsx
@@ -4,7 +4,6 @@ import { Layout } from '~/systems/Core';
 
 import { useCurrentVersion } from '../hooks/useCurrentVersion';
 import { useStableVersion } from '../hooks/useStableVersion';
-import { useVersion0204 } from '../hooks/useVersion0.20.4';
 
 const Repo = ({
   name,
@@ -37,7 +36,6 @@ const Repo = ({
 export const Version = () => {
   const { repos: currentRepos } = useCurrentVersion();
   const { repos: stableRepos } = useStableVersion();
-  const { repos: repos0204 } = useVersion0204();
 
   return (
     <Layout>
@@ -51,107 +49,7 @@ export const Version = () => {
                   fontSize="3xl"
                   css={{ textAlign: 'center' }}
                 >
-                  Master
-                </Text>
-                <Box.Flex
-                  gap="$10"
-                  direction="column"
-                  css={{ minWidth: 250, maxWidth: 250 }}
-                >
-                  {currentRepos.map((repo: any) => (
-                    <Box.Flex direction="column" key={repo.name + repo.version}>
-                      <Repo
-                        name={repo.name}
-                        version={repo.version}
-                        isCorrect={repo.isCorrect}
-                        isBig
-                      />
-                      <Box.Flex
-                        direction="column"
-                        css={{
-                          alignSelf: 'flex-end',
-                          '.fuel_Alert': {
-                            py: '$1',
-                            px: '$2',
-                          },
-                        }}
-                      >
-                        {repo.dependencies?.map((dependency: any) => (
-                          <Repo
-                            key={
-                              dependency.name + dependency.version + dependency
-                            }
-                            name={dependency.name}
-                            version={dependency.version}
-                            isCorrect={dependency.isCorrect}
-                          />
-                        ))}
-                      </Box.Flex>
-                    </Box.Flex>
-                  ))}
-                </Box.Flex>
-              </Box.Flex>
-            </Card.Body>
-          </Card>
-          <Card css={{ backgroundColor: '$intentsInfo2' }}>
-            <Card.Body>
-              <Box.Flex direction="column" gap="$10" css={{ flex: '1' }}>
-                <Text
-                  color="intentsInfo11"
-                  fontSize="3xl"
-                  css={{ textAlign: 'center' }}
-                >
-                  RC (BETA-5)
-                </Text>
-                <Box.Flex
-                  gap="$10"
-                  direction="column"
-                  css={{ minWidth: 250, maxWidth: 250 }}
-                >
-                  {currentRepos.map((repo: any) => (
-                    <Box.Flex direction="column" key={repo.name + repo.version}>
-                      <Repo
-                        name={repo.name}
-                        version={repo.version}
-                        isCorrect={repo.isCorrect}
-                        isBig
-                      />
-                      <Box.Flex
-                        direction="column"
-                        css={{
-                          alignSelf: 'flex-end',
-                          '.fuel_Alert': {
-                            py: '$1',
-                            px: '$2',
-                          },
-                        }}
-                      >
-                        {repo.dependencies?.map((dependency: any) => (
-                          <Repo
-                            key={
-                              dependency.name + dependency.version + dependency
-                            }
-                            name={dependency.name}
-                            version={dependency.version}
-                            isCorrect={dependency.isCorrect}
-                          />
-                        ))}
-                      </Box.Flex>
-                    </Box.Flex>
-                  ))}
-                </Box.Flex>
-              </Box.Flex>
-            </Card.Body>
-          </Card>
-          <Card css={{ backgroundColor: '$intentsInfo2' }}>
-            <Card.Body>
-              <Box.Flex direction="column" gap="$10" css={{ flex: '1' }}>
-                <Text
-                  color="intentsInfo11"
-                  fontSize="3xl"
-                  css={{ textAlign: 'center' }}
-                >
-                  RC (PATCH)
+                  ⚠️ BETA-5 ⚠️
                 </Text>
                 <Box.Flex
                   gap="$10"
@@ -206,48 +104,6 @@ export const Version = () => {
                     css={{ minWidth: 250, maxWidth: 250 }}
                   >
                     {stableRepos.map((repo: any) => (
-                      <Box.Flex
-                        direction="column"
-                        key={repo.name + repo.version}
-                      >
-                        <Repo
-                          name={repo.name}
-                          version={repo.version}
-                          isCorrect={repo.isCorrect}
-                          isBig
-                        />
-                        <Box.Flex
-                          direction="column"
-                          css={{
-                            alignSelf: 'flex-end',
-                            '.fuel_Alert': {
-                              py: '$1',
-                              px: '$2',
-                            },
-                          }}
-                        >
-                          {repo.dependencies?.map((dependency: any) => (
-                            <Repo
-                              key={
-                                dependency.name +
-                                dependency.version +
-                                dependency
-                              }
-                              name={dependency.name}
-                              version={dependency.version}
-                              isCorrect={dependency.isCorrect}
-                            />
-                          ))}
-                        </Box.Flex>
-                      </Box.Flex>
-                    ))}
-                  </Box.Flex>
-                  <Box.Flex
-                    gap="$10"
-                    direction="column"
-                    css={{ minWidth: 250, maxWidth: 250 }}
-                  >
-                    {repos0204.map((repo: any) => (
                       <Box.Flex
                         direction="column"
                         key={repo.name + repo.version}
---
+++ packages/app/src/systems/Bridge/utils/dependencies.ts
@@ -0,0 +1,336 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export async function fetchJsonUrl({ url }: { url: string }) {
+  try {
+    const response = await fetch(url);
+    const packageJson = await response.json();
+    return packageJson;
+  } catch (error) {
+    console.error(`Error fetching package version: ${error}`);
+    return '';
+  }
+}
+
+export function getVersionTag(version?: string) {
+  return version ? `v${version}` : 'master';
+}
+
+export async function getFuelsTsDependencies(repo: any) {
+  const tag = getVersionTag(repo?.version);
+  const packageJson = await fetchJsonUrl({
+    url: `https://raw.githubusercontent.com/FuelLabs/fuels-ts/${tag}/packages/fuels/package.json`,
+  });
+
+  const dependencies = await Promise.all(
+    (repo?.dependencies || []).map(async (dependency: any) => {
+      if (dependency.version) return dependency;
+
+      if (dependency.name === 'fuel-core') {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/FuelLabs/fuels-ts/${tag}/packages/fuel-core/VERSION`
+        );
+        const version = (await response.text()).trim();
+        return { ...dependency, version };
+      }
+
+      if (dependency.name === 'sway') {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/FuelLabs/fuels-ts/${tag}/packages/forc/VERSION`
+        );
+        const version = (await response.text()).trim();
+        return { ...dependency, version };
+      }
+
+      return dependency;
+    })
+  );
+
+  return { packageJson, dependencies };
+}
+
+export async function getFuelsWalletDependencies(repo: any) {
+  const tag = getVersionTag(repo?.version);
+
+  const packageJson = await fetchJsonUrl({
+    url: `https://raw.githubusercontent.com/FuelLabs/fuels-wallet/${tag}/packages/app/package.json`,
+  });
+
+  const dependencies = await Promise.all(
+    (repo.dependencies || []).map(async (dependency: any) => {
+      if (dependency.version) return dependency;
+      if (dependency.name === 'fuel-core') {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/FuelLabs/fuels-wallet/${tag}/docker/fuel-core/Dockerfile`
+        );
+        const dockerfile = (await response.text()).trim();
+        const regex = /FROM ghcr\.io\/fuellabs\/fuel-core:v([\d\.]+)/;
+        const match = dockerfile.match(regex);
+
+        if (match && match[1]) {
+          const version = match[1];
+          return { ...dependency, version };
+        }
+      }
+
+      if (dependency.name === 'sway') {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/FuelLabs/fuels-wallet/${tag}/fuel-toolchain.toml`
+        );
+        const toolchain = (await response.text()).trim();
+        const regex = /forc\s*=\s*"([\d\.]+)"/;
+        const match = toolchain.match(regex);
+
+        if (match && match[1]) {
+          const version = match[1];
+          return { ...dependency, version };
+        }
+      }
+
+      if (dependency.name === 'fuels-ts') {
+        const packageJson = await fetchJsonUrl({
+          url: `https://raw.githubusercontent.com/FuelLabs/fuels-wallet/${tag}/packages/app/package.json`,
+        });
+        const version = packageJson.dependencies?.fuels;
+        return { ...dependency, version };
+      }
+
+      return dependency;
+    })
+  );
+
+  return { packageJson, dependencies };
+}
+
+export async function getFuelsPortalDependencies(repo: any) {
+  const packageJson = await fetchJsonUrl({
+    url: `https://raw.githubusercontent.com/FuelLabs/fuels-portal/master/packages/app/package.json`,
+  });
+
+  const dependencies = await Promise.all(
+    (repo.dependencies || []).map(async (dependency: any) => {
+      if (dependency.version) return dependency;
+      // 'fuel-core' 'sway' 'fuels-ts'
+      if (dependency.name === 'fuel-core') {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/FuelLabs/fuels-portal/master/docker/fuel-core/Dockerfile`
+        );
+        const dockerfile = (await response.text()).trim();
+        const regex = /FROM ghcr\.io\/fuellabs\/fuel-core:v([\d\.]+)/;
+        const match = dockerfile.match(regex);
+
+        if (match && match[1]) {
+          const version = match[1];
+          return { ...dependency, version };
+        }
+      }
+
+      if (dependency.name === 'fuels-ts') {
+        const packageJson = await fetchJsonUrl({
+          url: `https://raw.githubusercontent.com/FuelLabs/fuels-portal/master/packages/app/package.json`,
+        });
+        const version = packageJson.dependencies.fuels;
+        return { ...dependency, version };
+      }
+
+      if (dependency.name === 'fuels-wallet') {
+        const packageJson = await fetchJsonUrl({
+          url: `https://raw.githubusercontent.com/FuelLabs/fuels-portal/master/packages/app/package.json`,
+        });
+        const version = packageJson.dependencies['@fuel-wallet/sdk'];
+        return { ...dependency, version };
+      }
+
+      if (dependency.name === 'fuel-bridge') {
+        const packageJson = await fetchJsonUrl({
+          url: `https://raw.githubusercontent.com/FuelLabs/fuels-portal/master/packages/app/package.json`,
+        });
+        const version =
+          packageJson.dependencies['@fuel-bridge/message-predicates'];
+        return { ...dependency, version };
+      }
+
+      return dependency;
+    })
+  );
+
+  return { packageJson, dependencies };
+}
+
+export async function getFuelBridgeDependencies(repo: any) {
+  const tag = getVersionTag(repo?.version);
+
+  const packageJson = await fetchJsonUrl({
+    url: `https://raw.githubusercontent.com/FuelLabs/fuel-bridge/${tag}/packages/message-predicates/package.json`,
+  });
+
+  const dependencies = await Promise.all(
+    (repo.dependencies || []).map(async (dependency: any) => {
+      if (dependency.version) return dependency;
+      // 'fuel-core' 'sway' 'fuels-ts'
+      if (dependency.name === 'fuel-core') {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/FuelLabs/fuel-bridge/${tag}/docker/fuel-core/Dockerfile`
+        );
+        const dockerfile = (await response.text()).trim();
+        const regex = /FROM ghcr\.io\/fuellabs\/fuel-core:v([\d\.]+)/;
+        const match = dockerfile.match(regex);
+
+        if (match && match[1]) {
+          const version = match[1];
+          return { ...dependency, version };
+        }
+      }
+
+      if (dependency.name === 'fuels-ts') {
+        const packageJson = await fetchJsonUrl({
+          url: `https://raw.githubusercontent.com/FuelLabs/fuel-bridge/${tag}/packages/integration-tests/package.json`,
+        });
+        const version = packageJson.devDependencies.fuels;
+        return { ...dependency, version };
+      }
+
+      return dependency;
+    })
+  );
+
+  return { packageJson, dependencies };
+}
+
+export async function getSwayDependencies(repo: any) {
+  const tag = getVersionTag(repo?.version);
+
+  const response = await fetch(
+    `https://raw.githubusercontent.com/FuelLabs/sway/${tag}/forc/Cargo.toml`
+  );
+  const toolchain = (await response.text()).trim();
+  const regex = /version\s*=\s*"([^"]+)"/;
+  const match = toolchain.match(regex);
+
+  let repoVersion: string = '';
+  if (match && match[1]) {
+    const version = match[1];
+    repoVersion = version;
+  }
+
+  const dependencies = await Promise.all(
+    (repo.dependencies || []).map(async (dependency: any) => {
+      if (dependency.version) return dependency;
+      // 'fuel-core' 'sway' 'fuels-ts'
+      if (dependency.name === 'fuel-core') {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/FuelLabs/sway/${repoVersion}/test/src/sdk-harness/Cargo.toml`
+        );
+        const tomlFile = (await response.text()).trim();
+        const regex = /fuel-core\s*=\s*\{[^}]*version\s*=\s*"([^"]+)"/;
+        const match = tomlFile.match(regex);
+
+        if (match && match[1]) {
+          const version = match[1];
+
+          return { ...dependency, version };
+        }
+      }
+    })
+  );
+
+  return { version: repoVersion, dependencies };
+}
+
+export async function getReposAndDependencies(rawRepos: any) {
+  const repos = await Promise.all(
+    rawRepos.map(async (repo: any) => {
+      if (repo.name === 'sway') {
+        const { version, dependencies } = await getSwayDependencies(repo);
+
+        return {
+          ...repo,
+          dependencies,
+          version,
+        };
+      }
+
+      if (repo.name === 'fuels-ts') {
+        const { packageJson, dependencies } = await getFuelsTsDependencies(
+          repo
+        );
+
+        return {
+          ...repo,
+          dependencies,
+          version: packageJson.version,
+        };
+      }
+
+      if (repo.name === 'fuels-wallet') {
+        const { packageJson, dependencies } = await getFuelsWalletDependencies(
+          repo
+        );
+
+        return {
+          ...repo,
+          dependencies,
+          version: repo.version || packageJson.version,
+        };
+      }
+
+      if (repo.name === 'fuels-portal') {
+        const { packageJson, dependencies } = await getFuelsPortalDependencies(
+          repo
+        );
+
+        return {
+          ...repo,
+          dependencies,
+          version: packageJson.version,
+        };
+      }
+
+      if (repo.name === 'fuel-bridge') {
+        const { packageJson, dependencies } = await getFuelBridgeDependencies(
+          repo
+        );
+
+        return {
+          ...repo,
+          dependencies,
+          version: packageJson.version,
+        };
+      }
+
+      return repo;
+    })
+  );
+
+  const analyzedRepos = repos.reduce<(typeof repos)[0][]>((prev: any, repo) => {
+    if (repo.isCorrect) return [...prev, repo];
+
+    const dependencies = repo.dependencies?.map((dependency: any) => {
+      if (dependency.isCorrect) return dependency;
+
+      const rootRepo: any = prev.find(
+        (prevRepo: any) => prevRepo.name === dependency.name
+      );
+
+      return {
+        ...dependency,
+        isCorrect:
+          dependency.version === rootRepo?.version && rootRepo.isCorrect,
+      };
+    });
+
+    const isRepoCorrect = dependencies?.every(
+      (dependency: any) => dependency.isCorrect
+    );
+
+    return [
+      ...prev,
+      {
+        ...repo,
+        isCorrect: isRepoCorrect,
+        dependencies,
+      },
+    ];
+  }, []);
+
+  return analyzedRepos;
+}
