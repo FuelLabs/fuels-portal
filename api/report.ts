cff72dd65b6b60452bc4ac08d43d8ce5cd362f96
Luiz Estácio <luizstacio@gmail.com>
2023-12-14 17:09:36 -0300

    chore: add CSP policy

--- api/report.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(
-  request: VercelRequest,
-  response: VercelResponse
-) {
-  console.log(request.body);
-  return response.status(200).json({ data: 'ok' });
-}
