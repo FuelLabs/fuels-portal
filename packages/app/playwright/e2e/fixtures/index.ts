a5238cdcde5c2ad233bdfdca5d17b43c7a9c97c4
LuizAsFight <felipebolsonigomes@gmail.com>
2024-01-17 17:01:26 -0300

    fix: wallet version e2e

--- packages/app/playwright/e2e/fixtures/index.ts
+++ packages/app/playwright/e2e/fixtures/index.ts
@@ -20,7 +20,7 @@ export const test = base.extend<{
     // required for synpress
     global.expect = expect;
     // download fuel wallet
-    const fuelPathExtension = await downloadFuel('0.13.3');
+    const fuelPathExtension = await downloadFuel('0.14.2');
     // download metamask
     const metamaskPath = await prepareMetamask(
       process.env.META_MASK_VERSION || '10.25.0'
